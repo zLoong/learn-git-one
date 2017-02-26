e31463fa4a85c385e691d94632958035b0536823
yuloong <384787230@qq.com>
2017-02-26 10:43:29 +0800

    git one 8 index.js

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     function add(a, b) {
         return (a + b);
     }
+    function getWindowSize(){
+        return {
+            width:window.innerWidth,
+            height:window.innerHeight
+        }
+    }
 }, false);
